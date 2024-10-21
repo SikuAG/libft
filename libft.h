9b60b374e8fa5ef6116a27bcaf8a31b21073b4d7
SikuAG <ajimcon@protonmail.com>
2024-10-21 11:09:25 +0200

    Update libft.h
    Added limits.h
    Added coments to organice blocks of content

--- libft.h
+++ libft.h
@@ -13,12 +13,10 @@
 #ifndef LIBFT_H
 # define LIBFT_H
 
-# define FT_INT_MIN -2147483648
-# define FT_INT_MAX 2147483647
-
 # include <stdlib.h>
 # include <stdio.h>
 # include <unistd.h>
+# include <limits.h>
 # include <string.h>
 
 typedef struct s_list
@@ -27,7 +25,7 @@ typedef struct s_list
 	struct s_list	*next;
 }			t_list;
 
-/*part 1*/
+/* ********************************** Part 1 ******************************** */
 //ctype.h
 int		ft_isalnum(int c);
 int		ft_isalpha(int c);
@@ -59,9 +57,9 @@ char	*ft_strdup(const char *s); /*requires malloc()*/
 
 //stdlib.h
 int		ft_atoi(const char *nptr);
-void	*ft_calloc(size_t number, size_t size); /*requires malloc()*/
+void	*ft_calloc(size_t number, size_t size); //requires malloc()
 
-/*part 2*/
+/* ********************************* Part 2 ********************************* */
 char	*ft_substr(char const *s, unsigned int start, size_t len);
 char	*ft_strjoin(char const *s1, char const *s2);
 char	*ft_strtrim(char const *s1, char const *set);
@@ -74,7 +72,7 @@ void	ft_putstr_fd(char *s, int fd);
 void	ft_putendl_fd(char *s, int fd);
 void	ft_putnbr_fd(int n, int fd);
 
-/*bonus*/
+/* ********************************* Bonus ********************************** */
 t_list	*ft_lstnew(void *content);
 void	ft_lstadd_front(t_list **lst, t_list *new);
 int		ft_lstsize(t_list *lst);
